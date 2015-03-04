b62f9f29b78b670dfcf8e669331b267281f5a4b7
goldnarms <arnstej@gmail.com>
2015-03-04 12:36:48 +0100

    Added MVC Solution

---
+++ src/Boilerplate/Boilerplate.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Boilerplate.Web.Startup))]
+namespace Boilerplate.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
